37c7ef184dafcc6115d4666e6eee97135837716d
Nahid <syed.ah.nahid@gmail.com>
2024-12-18 15:27:10 +0600

    make phone number nulable and remove profile picture

--- Models/User.cs
+++ Models/User.cs
@@ -6,13 +6,12 @@ namespace IdentityX.Models
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-        public string ProfilePicture { get; set; }
+        public string? PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsEmailVerified { get; set; }   
         public bool IsTwoFactorEnabled { get; set; }
-        public string TwoFactorSecret { get; set; }
+        public string? TwoFactorSecret { get; set; }
 
         public ICollection<UserConfirmationTokens> UserConfirmationTokens { get; set; }
         public ICollection<UserSessions> UserSessions { get; set; }
